4572825a0666dd2286266a205356f459c13c8bcc
eppleton <info@eppleton.de>
2014-06-26 12:33:33 +0200

    added sample how to use in JavaFX

---
+++ l4jfxdemo/src/main/java/org/apidesign/html/demo/l4jfxdemo/MainApp.java
@@ -0,0 +1,98 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2014 Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.apidesign.html.demo.l4jfxdemo;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import net.java.html.boot.fx.FXBrowsers;
+import org.apidesign.html.leaflet.api.LatLng;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        final MapView map = new MapView();
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(map);
+
+        // a regular JavaFX ListView 
+        ListView<Address> listView = new ListView<Address>();
+        listView.getItems().addAll(new Address("Toni", new LatLng(48.1322840, 11.5361690)),
+                new Address("Jarda", new LatLng(50.0284060, 14.4934400)),
+                new Address("JUG Münster", new LatLng(51.94906770000001, 7.613701100000071)));
+        // we listen for the selected item and update the map accordingly
+        // as a demo of how to interact between JavaFX and DukeScript
+        listView.getSelectionModel().selectedItemProperty().addListener(
+                (ObservableValue<? extends Address> ov, Address old_val, final Address new_val) -> {
+                    FXBrowsers.runInBrowser(map.getWebView(), new Runnable() {
+                        
+                        @Override
+                        public void run() {
+                            // use the Java api to manipulate the view
+                            map.getLeaflet().setView(new_val.getPos(), 20);
+                            map.getLeaflet().openPopup(new_val.getPos(),"Here is "+new_val);
+                        }
+                    });
+        });
+
+        borderPane.setLeft(listView);
+        Scene scene = new Scene(borderPane);
+
+        stage.setTitle(
+                "JavaFX and DukeScript");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static class Address {
+
+        private final String name;
+        private final LatLng pos;
+
+        public Address(String name, LatLng pos) {
+            this.name = name;
+            this.pos = pos;
+        }
+
+        public LatLng getPos() {
+            return pos;
+        }
+
+        @Override
+        public String toString() {
+            return name; 
+        }
+
+        
+        
+    }
+
+}
---
+++ l4jfxdemo/src/main/java/org/apidesign/html/demo/l4jfxdemo/MapView.java
@@ -0,0 +1,92 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2014 Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.apidesign.html.demo.l4jfxdemo;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
+import net.java.html.boot.fx.FXBrowsers;
+import org.apidesign.html.leaflet.api.LatLng;
+import org.apidesign.html.leaflet.api.Leaflet;
+
+/**
+ * A simple example View of how to embed DukeScript in a JavaFX Application. In
+ * this example we use leaflet4j https://github.com/jtulach/leaflet4j.
+ *
+ *
+ * @author antonepple
+ */
+public class MapView extends StackPane {
+
+    private final WebView webView;
+    private Leaflet leaflet;
+
+    public MapView() {
+        // we define a regular JavaFX WebView that DukeScript can use for rendering
+        webView = new WebView();
+        getChildren().add(webView);
+        
+        // FXBrowsers loads the associated page into the WebView and runs our 
+        // code. 
+        FXBrowsers.load(webView, MapView.class.getResource("/pages/index.html"), new Runnable() {
+
+            @Override
+            public void run() {
+                // Here we define that the map is rendered to a div with id="map" 
+                // in our index.html.
+                // This can only be done after the page is loaded and the context is initialized.
+                leaflet = Leaflet.map("map");
+                
+                // from here we just use the Leaflet API to show some stuff on the map
+                leaflet.setView(new LatLng(51.505, -0.09), 13);
+                leaflet.addTileLayer(
+                        "https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png",
+                        "Map data &copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, "
+                        + "<a href='http://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, "
+                        + "Imagery © <a href='http://mapbox.com'>Mapbox</a>",
+                        18,
+                        "eppleton.ia9c2p12"
+                );
+                // sample code showing how to use the Java API
+                leaflet.addCircle(
+                        new LatLng(51.508, -0.11), 500, "red", "#f03", 0.5
+                ).bindPopup("I am a Circle");
+                leaflet.addPolygon(
+                        new LatLng(51.509, -0.08),
+                        new LatLng(51.503, -0.06),
+                        new LatLng(51.51, -0.047)
+                ).bindPopup("I am a polygon");
+
+            }
+        });
+    }
+
+    public Leaflet getLeaflet() {
+        return leaflet;
+    }
+
+    public WebView getWebView() {
+        return webView;
+    }
+
+}
